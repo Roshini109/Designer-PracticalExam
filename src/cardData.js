8d658648b4b62c70c5d036f7bba1d542c1310e2d
Roshini Dias <jbrnd21@gmail.com>
2023-12-17 23:50:39 +0530

    update Create card

---
+++ src/cardData.js
@@ -0,0 +1,41 @@
+const cardData = [
+    {
+      id: 1,
+      imageUrl: "https://www.hilton.com/im/en/NoHotel/18127576/family-pool-day-0199-v1.jpg?impolicy=crop&cw=2000&ch=1367&gravity=NorthWest&xposition=0&yposition=66&rw=1536&rh=1050",
+      alt: "image1",
+      title: "ADVANCE PURCHASE DISCOUNT- SAVE UP TO 17%",
+    },
+    {
+      id: 2,
+      imageUrl: "https://www.hilton.com/im/en/MBJRHHF/16722849/2021-lifestyle-shoot-at-hilton-rose-hall-resort-spa-an-all-inclusive-resort.jpg?impolicy=crop&cw=4874&ch=3333&gravity=NorthWest&xposition=62&yposition=0&rw=1536&rh=1050",
+      alt: "image2",
+      title: "JOIN HILTON HONORS & GET UP TO 4,500 POINTS",
+    },
+    {
+      id: 3,
+      imageUrl: "https://www.hilton.com/im/en/NoHotel/15196711/shutterstock-269455079.jpg?impolicy=crop&cw=4874&ch=3333&gravity=NorthWest&xposition=62&yposition=0&rw=1536&rh=1050",
+      alt: "image3",
+      title: "MULTIPLY YOUR POINTS",
+    },
+    {
+      id: 4,
+      imageUrl: "https://www.hilton.com/im/en/NoHotel/13984175/cheers-5000x3333.jpg?impolicy=crop&cw=4874&ch=3333&gravity=NorthWest&xposition=62&yposition=0&rw=1536&rh=1050",
+      alt: "image4",
+      title: "FREE BONUS POINTS AT NEW HOTELS",
+    },
+    {
+      id: 5,
+      imageUrl: "https://www.hilton.com/im/en/NoHotel/19032520/shutterstock-1991645033.jpg?impolicy=crop&cw=5000&ch=3333&gravity=NorthWest&xposition=0&yposition=0&rw=1536&rh=1024",
+      alt: "image5",
+      title: "PARK & STAY",
+    },
+    {
+      id: 6,
+      imageUrl: "https://www.hilton.com/im/en/SINORHI/14530273/f-por-157-heroimage-crop3-2.jpg?impolicy=crop&cw=1500&ch=1000&gravity=NorthWest&xposition=0&yposition=0&rw=1536&rh=1024",
+      alt: "image6",
+      title: "EXPERIENCE THE STAY",
+    }
+  ];
+  
+  export default cardData;
+  
